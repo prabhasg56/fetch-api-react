736510608ba5a4a806ccfa173dac4ee3bba58456
Prabhakar Mandal <prabhask856@gmail.com>
2023-07-11 12:24:35 +0530

    promises handled by async and await

--- src/App.js
+++ src/App.js
@@ -5,46 +5,44 @@ import "./App.css";
 
 function App() {
   const [moviesData, setMoviesData] = useState([]);
-  const dummyMovies = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18",
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-    {
-      id: 3,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-  ];
 
-  const moviesDataHandler = () => {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-       const transformedMovies = data.results.map((moviesData, ind) => {
-          return {
-            id: moviesData.episode_id,
-            title: moviesData.title,
-            openingText: moviesData.opening_crawl,
-            releaseDate: moviesData.release_date,
-          };
-        });
-        setMoviesData(transformedMovies);
-      });
+  // const moviesDataHandler = () => {
+  //   fetch("https://swapi.dev/api/films")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //      const transformedMovies = data.results.map((moviesData, ind) => {
+  //         return {
+  //           id: moviesData.episode_id,
+  //           title: moviesData.title,
+  //           openingText: moviesData.opening_crawl,
+  //           releaseDate: moviesData.release_date,
+  //         };
+  //       });
+  //       setMoviesData(transformedMovies);
+  //     });
+  // };
+
+
+  // promises handled by async and await //
+  const moviesDataHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+
+    const jsonResponse = await response.json();
+
+    const transformedMovies = jsonResponse.results.map((moviesData, ind) => {
+      return {
+        id: moviesData.episode_id,
+        title: moviesData.title,
+        openingText: moviesData.opening_crawl,
+        releaseDate: moviesData.release_date,
+      };
+    });
+
+    setMoviesData(transformedMovies);
   };
 
-  console.log(moviesData)
   return (
     <React.Fragment>
       <section>
